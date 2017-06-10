93a5483dfd1541bab41ba7912c9058690ad238e2
Masad, Ofri <ofri.masad@emc.com>
2017-06-10 18:21:18 +0300

    added comments

--- OrdinalClassifier.py
+++ OrdinalClassifier.py
@@ -12,18 +12,35 @@ warnings.filterwarnings("ignore")
 
 class OrdinalClassifier(object):
     def __init__(self, classifier, orderedClassList):
+        """
+        initiating the class with prediction classifier and the ordinal classes order
+        :param classifier: classifier to perform the prediction
+        :param orderedClassList: list of ordered ordinal classes
+        """
         self.classifier = classifier
         self.orderedClassList = orderedClassList
         self.models = []
 
     def fit(self, X, y):
+        """
+        fit the ordinal classifier
+        :param X: training datas
+        :param y: training labels
+        :return: the fitted model
+        """
         datasets = self.generateDatasets(y)
         for dataset in datasets:
             tmpclf = clone(self.classifier)
             tmpclf.fit(X, dataset)
             self.models.append(tmpclf)
+        return self
 
     def predict(self, X):
+        """
+        predict the given data
+        :param X: test data
+        :return: list of predictions of the given data
+        """
         predictions = []
         for x in X.values:
             index, bestProb = -1, -1
@@ -41,6 +58,12 @@ class OrdinalClassifier(object):
         return predictions
 
     def score(self, X, y):
+        """
+        calculate the accuracy of the model on the given data
+        :param X: test data
+        :param y: test labels
+        :return: the accuracy of the model on the given data
+        """
         predictions = self.predict(X)
         correct = 0
         for i in range(len(y)):
@@ -49,15 +72,18 @@ class OrdinalClassifier(object):
         return float(correct) / len(y)
 
     def generateDatasets(self, y):
+        """
+        create list of different labels according the paper algorithm
+        :param y: original labels
+        :return: list of labels for each dataset as in the paper
+        """
         datasets = []
         for i, c1 in enumerate(self.orderedClassList[:-1]):
             classes = []
             for c2 in self.orderedClassList[:i + 1]:
                 classes.append(c2)
-
             def f(x):
                 return 0 if x in classes else 1
-
             newds = y.copy(True)[0].apply(f)
             datasets.append(newds)
         return datasets
