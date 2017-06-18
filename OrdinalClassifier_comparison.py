3cdfabf8a59aadc779e7e5298d6a489bdc508824
Ofri <ofrik89@gmail.com>
2017-06-18 17:48:32 +0300

    added score time, finished the charts and report

--- OrdinalClassifier_comparison.py
+++ OrdinalClassifier_comparison.py
@@ -3,6 +3,8 @@ __author__ = 'Ofri'
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.base import clone
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold
@@ -63,21 +65,27 @@ if __name__ == "__main__":
                 ("Online News Popularity", popularity_df_X, regressionToOrdinal(popularity_df_Y.values))]
 
     kf = KFold(n_splits=5)
-    clfs = [("KNeighborsClassifier",KNeighborsClassifier),("RandomForestClassifier",RandomForestClassifier),("MLPClassifier",MLPClassifier)]
+    clfs = [("SVC", SVC(probability=True)), ("RandomForestClassifier", RandomForestClassifier(n_jobs=-1)),
+            ("MLPClassifier", MLPClassifier())]
+
+
     def checkClassifier(clf):
         for dataset in datasets:
-            print "comparing %s using %s" % (dataset[0],clf[0])
+            t = datetime.now()
+            print "comparing %s using %s" % (dataset[0], clf[0])
             X = dataset[1]
-            print "has %s train records"%X.shape[0]
+            print "has %s train records" % X.shape[0]
             y = dataset[2]
             originalScores = []
             originalFitTime = []
+            originalScoreTime = []
             ordinalScores = []
             ordinalFitTime = []
+            ordinalScoreTime = []
             for train_index, test_index in kf.split(X):
                 X_train, X_test = X.iloc[train_index], X.iloc[test_index]
                 y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-                estimator = clf[1](n_jobs=-1)
+                estimator = clf[1]
                 ordinal = OrdinalClassifier(estimator, [1, 2, 3])
                 t0 = datetime.now()
                 estimator.fit(X_train, y_train)
@@ -85,10 +93,17 @@ if __name__ == "__main__":
                 t0 = datetime.now()
                 ordinal.fit(X_train, y_train)
                 ordinalFitTime.append((datetime.now() - t0).total_seconds())
+                t0 = datetime.now()
                 originalScores.append(estimator.score(X_test, y_test))
+                originalScoreTime.append((datetime.now() - t0).total_seconds())
+                t0 = datetime.now()
                 ordinalScores.append(ordinal.score(X_test, y_test))
-            print "\noriginal mean score: %s with mean time of %s seconds\nordinal mean score: %s with mean time of %s seconds\n\n" % (
-            np.mean(originalScores),np.mean(originalFitTime),np.mean(ordinalScores),np.mean(ordinalFitTime))
+                ordinalScoreTime.append((datetime.now() - t0).total_seconds())
+            print "original mean score: %s with mean time of %s seconds and mean score time of %s seconds\nordinal mean score: %s with mean time of %s seconds and mean score time of %s seconds" % (
+                np.mean(originalScores), np.mean(originalFitTime), np.mean(originalScoreTime), np.mean(ordinalScores),
+                np.mean(ordinalFitTime), np.mean(ordinalScoreTime))
+            print "the entire thing took %s seconds\n\n" % ((datetime.now() - t).total_seconds())
+
 
     for clf in clfs:
         checkClassifier(clf)
